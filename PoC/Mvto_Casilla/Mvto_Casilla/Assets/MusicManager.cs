b67dc37655a76499ec37a030b155ea6e275e62ba
lcosteroucm <lcostero@ucm.es>
2016-03-11 13:20:08 +0100

    Prueba de concepto para simular los ritmos y el movimiento uniforme. Lo próximo es hacer que se mueva algo de acuerdo al ritmo, no al uniforme º

---
+++ PoC/Mvto_Casilla/Mvto_Casilla/Assets/MusicManager.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class MusicManager : MonoBehaviour {
+	public static MusicManager S; //Singleton
+
+
+	public List<double> rhythm;
+	private int currentIdx = 0;
+
+	/** Marca de tiempo para realizar los cálculos a partir de ella (tiempo 0).
+	 * Invocar la funcion StartTime() antes de comenzar la ejecución del tiempo
+	 */
+	public float relativeTime=0;
+
+	void Awake () {
+		S = this;
+		currentIdx=0;
+
+		initMusic();
+		//Chapuza para no tener que calcular cuando nos salimos
+		rhythm.Add(1e9);
+	}
+		
+	private void initMusic(){
+		rhythm = new List<double>();
+
+		rhythm.Add(1.0);
+		rhythm.Add(2.0);
+		rhythm.Add(3.0);
+		rhythm.Add(4.5);
+		rhythm.Add(6.0);
+		rhythm.Add(7.0);
+	}
+
+
+	double getCurrentRithmPulse(){
+		//Sin la chapuza del final sería algo así
+		//return (currentIdx >= rhythm.Count) ? 1e9 :	rhythm[currentIdx];
+
+		return rhythm[currentIdx];
+	}
+
+	double getNextRithmPulse(){
+		//Sin la chapuza del final sería algo así
+		//return (currentIdx >= rhythm.Count-1) ? 1e9 : rhythm[currentIdx+1];
+
+		return rhythm[currentIdx+1];
+	}
+			
+	public void StartTime(){
+		relativeTime = Time.time;
+		currentIdx = 0;
+	}
+
+	void Update(){
+		/**
+		 * TODO: Aquí habría que mirar si es mejor usar Time.time; Time.fixedtime; Time.realtime; .....
+		 **/
+		if(relativeTime == 0)
+			return;
+		
+		float currentTime = Time.time - relativeTime;
+
+		if(rhythm[currentIdx+1] <= currentTime)
+			currentIdx ++;
+	}
+}
