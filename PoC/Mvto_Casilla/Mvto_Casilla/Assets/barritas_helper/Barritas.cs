ceb427ff1c91ac23c53fd66a20a640bb84724a09
Usuario Local <Usuario Local>
2016-03-15 15:56:01 +0100

    Creo que me faltaba esto por hacer

---
+++ PoC/Mvto_Casilla/Mvto_Casilla/Assets/barritas_helper/Barritas.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * La idea loca de esta clase es que pinta barritas a alturas distintas según el ritmo.
+ * Las marcs de tiempo las lee del MusicManager, por lo que la función principal estára en el Start (intentando evitar
+ * condiciones de carrera entre los métodos Awake de las dos clases).
+ */
+public class Barritas : MonoBehaviour {
+	public GameObject barrita;
+	public GameObject guia_prefab;
+	public static float verticalDistance = 1;
+	public static float widthScreen = 200;
+
+	public bool ________________________________;
+
+	public GameObject anchor;
+	public GameObject guia;
+
+	private float widthPerSecond;
+
+	void Start(){
+		if(this.anchor==null){
+			anchor = new GameObject();
+			anchor.transform.position = new Vector3(0, 100, 0);
+			anchor.name = "Anchor de las barritas";
+		}
+
+		iniciaBarras();
+		iniciaGuia();
+
+		Invoke("IniciaEspectaculo", 2);
+	}	
+
+	void IniciaEspectaculo(){
+		print("Que comiencen los juegos del hambre!!");
+		MusicManager.S.StartTime();
+	}
+
+
+	void iniciaGuia(){
+		guia = Instantiate(guia_prefab);
+		guia.transform.parent = anchor.transform;
+		guia.transform.localPosition = new Vector3(0,0,0);
+
+	}
+
+
+
+	void iniciaBarras(){
+		List<double> rhythm = MusicManager.S.rhythm;
+
+		//Calculamos a cuanto espacio de la pantalla corresponde un segundo de la melodia
+		widthPerSecond = widthScreen / (float)(rhythm[rhythm.Count-2]);
+
+
+		//pintar las barritas por cada golpe de ritmo
+		short level = -1;
+		float start=0, end;
+
+		for(int i=0; i<rhythm.Count-1; i++){
+			end = (float)rhythm[i];
+
+			//barrita a altura level, de tamaño (end-start)*widthPerSecond
+			GameObject go = Instantiate(barrita);
+			go.transform.parent = anchor.transform;
+
+			float width = (end-start)*widthPerSecond;
+			go.transform.localPosition = new Vector3((start*widthPerSecond) + (width/2), level*verticalDistance, 0);
+			go.transform.localScale = new Vector3(width,1,1);
+
+			//preparar la siguiente vuelta
+			level *= -1;
+			start = end;
+		}
+	}
+
+
+	void Update(){
+		if(MusicManager.S.relativeTime == 0)
+			return;
+
+		float elapsedtime = Time.time - MusicManager.S.relativeTime;
+		//Aunque es muy burro, como se supone que el metodo Update se va a llamar muchas veces, nos movemos directamente
+		//en vez de hacer la interpolacion y movernos como se debería.
+		guia.transform.position = new Vector3(elapsedtime*widthPerSecond, 100, 0);
+	}
+}
---
+++ PoC/Mvto_Casilla/Mvto_Casilla/Assets/mvto/Metodo1.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+/**
+ * MÉTODO 1 de movimiento:
+ * Este método es similar a como funcionan las barritas del helper. 
+ * La clase solicita el array de tiempos de ritmos en la constructora, y se mueve
+ * de acuerdo a este array, sin realiar ninguna comunicación extra con alguna otra clase
+ * 
+ * El tiempo de inicio (relativeTime), podría acceder a través de la clase MusicManager, o 
+ * realizar una copia (cuando != 0). La gracia está en que al usar un tiempo relativo al frame
+ * actual, todos los métodos time llamados en el mismo frame tienen el mismo valor,
+ * independientemente de si un método Update realiza mucho trabajo o poco.
+ **/
+
+public class Metodo1 : MonoBehaviour {
+    // Relativo a la sincronizacion
+    private List<double> rhythm;
+    private float relativeTime=0;
+
+    //Relativo al movimiento
+    public Vector3 poi; //objetivo
+    public int idx = 0;
+
+    //chapucero
+    int mvto = 4;
+
+
+	void Start () {
+        this.rhythm = MusicManager.S.rhythm;
+        poi = new Vector3(0.0f, 0.0f, 0.0f);
+	}
+	
+	void Update () {
+        if (relativeTime == 0){
+            relativeTime = MusicManager.S.relativeTime;
+            if(relativeTime == 0) return;
+        }
+
+
+        float currentTime = Time.time-relativeTime;
+       //comprobamos si estamos en el proximo pulso
+        if (rhythm[idx + 1] <= currentTime)
+            actualizaObjetivo();
+
+        //interpolacion lineal. Calcular cuanto nos tenemos que mover.
+        float ttime = (float)rhythm[idx + 1] - currentTime;
+        float nTicks = ttime / Time.deltaTime; //numero ticks hasta objetivo
+
+        float dist = poi.x - this.transform.position.x; //distancia hasta objetivo
+        float avance = dist / nTicks; //lo que hay que avanzar en este tick
+
+        this.transform.position = new Vector3(transform.position.x + avance, 0, 0);
+	}
+
+
+    void actualizaObjetivo() {
+        this.mvto *= -1;
+        this.poi += new Vector3(2 * this.mvto, 0, 0);
+    }
+}
